e890c84842fa02377f0c9d3959a598d0e7cb93e0
starosta2123 <starosta2218@yandex.ru>
2023-01-13 10:42:40 +0400

    #1 rename

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import openai
 import telebot
 
-openai.api_key = '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'
-bot=telebot.TeleBot("5904307834:AAF0TvFZkTCWz17LifeTXN0YxUM_wPC3qLo")
+openai.api_key = 'API_key OPENAI'
+bot=telebot.TeleBot("API_rey telegrambot")
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
